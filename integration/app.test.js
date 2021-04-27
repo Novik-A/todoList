f0d5ded66a268aefe534fc1dbac3e92e6cbf358a
Novik-A <antosh2878@gmail.com>
2021-04-27 11:34:16 +0300

    add storybook and snapshot

--- integration/app.test.js
+++ integration/app.test.js
@@ -9,6 +9,28 @@ describe('addItemForm', () => {
     });
 });
 
+describe('appWithRedux', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolist-appwithredux--app-with-redux-example&args=');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('taskIsDone', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolist-task--task-is-done-example&args=');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
 describe('editableSpan', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
@@ -20,3 +42,13 @@ describe('editableSpan', () => {
     });
 });
 
+describe('taskIsNotDone', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolist-task--task-is-not-done-example&args=');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
